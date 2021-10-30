3338e55702939c404c99c340c407c32631edf625
John T. Wodder II <git@varonathe.org>
2021-10-30 18:55:20 +0000

    Split up code

---
+++ ghissues/__main__.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+__python_requires__ = '~= 3.7'
+__requires__ = ['requests ~= 2.20']
+from   email.message import EmailMessage
+import json
+from   pathlib       import Path
+import subprocess
+import time
+from   .client       import GitHub
+from   .types        import RepoRemovedEvent
+
+USER = 'jwodder'
+RECIPIENT = 'REDACTED'
+SUBJECT = 'New issues on your GitHub repositories'
+SKIP_SELF_REPORTED = True
+
+TOKEN_FILE = Path.home() / '.github'
+STATE_FILE = Path.home() / '.varlib' / 'ghissues.json'
+
+def main():
+    gh = GitHub(TOKEN_FILE)
+    try:
+        with STATE_FILE.open() as fp:
+            state = json.load(fp)
+    except FileNotFoundError:
+        state = {}
+    events = []
+    new_state = {}
+    for repo in gh.get_user_repos(USER):
+        try:
+            repo_state = state.pop(repo.id)
+        except KeyError:
+            repo_state = {
+                "issues": repo.issues.get_latest_cursor(),
+                "prs": repo.prs.get_latest_cursor(),
+                "discussions": repo.discussions.get_latest_cursor(),
+            }
+            events.append(repo.new_event)
+        else:
+            ### TODO: Cut down on code duplication here:
+            issues, new_cursor = repo.issues.get_new(repo_state["issues"])
+            if new_cursor is not None:
+                repo_state["issues"] = new_cursor
+            events.extend(
+                i for i in issues if i.author != USER or not SKIP_SELF_REPORTED
+            )
+            prs, new_cursor = repo.prs.get_new(repo_state["prs"])
+            if new_cursor is not None:
+                repo_state["prs"] = new_cursor
+            events.extend(
+                p for p in prs if p.author != USER or not SKIP_SELF_REPORTED
+            )
+            try:
+                discurse = repo_state["discussions"]
+            except KeyError:
+                discurse = repo_state["discussions"] = repo.discussions.get_latest_cursor()
+            discs, new_cursor = repo.discussions.get_new(discurse)
+            if new_cursor is not None:
+                repo_state["discussions"] = new_cursor
+            events.extend(
+                d for d in discs if d.author != USER or not SKIP_SELF_REPORTED
+            )
+        repo_state["fullname"] = repo.fullname
+        new_state[repo.id] = repo_state
+    for repo_state in state.values():
+        events.append(RepoRemovedEvent(
+            timestamp = nowstamp(),
+            repo_fullname = repo_state["fullname"],
+        ))
+    events.sort()
+    if events:
+        report_events(events)
+    STATE_FILE.parent.mkdir(parents=True, exist_ok=True)
+    STATE_FILE.write_text(json.dumps(new_state))
+
+def report_events(events):
+    #print('\n\n'.join(map(str, events)))
+    msg = EmailMessage()
+    msg['Subject'] = SUBJECT
+    msg['To'] = RECIPIENT
+    msg.set_content('\n\n'.join(map(str, events)))
+    subprocess.run(['sendmail', '-i', '-t'], input=bytes(msg), check=True)
+
+
+def nowstamp():
+    return time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+
+if __name__ == '__main__':
+    main()
--- ghissues/client.py
+++ ghissues/client.py
@@ -1,90 +1,11 @@
-#!/usr/bin/python3
-__python_requires__ = '~= 3.7'
-__requires__ = ['requests ~= 2.20']
-from   collections   import namedtuple
-from   dataclasses   import dataclass
-from   email.message import EmailMessage
 import json
-from   pathlib       import Path
-import subprocess
-import time
-from   typing        import ClassVar
+from pathlib import Path
 import requests
+from .types import NewIssueEvent, NewPREvent, NewDiscussEvent, NewRepoEvent
 
-USER = 'jwodder'
-RECIPIENT = 'REDACTED'
-SUBJECT = 'New issues on your GitHub repositories'
-SKIP_SELF_REPORTED = True
 PAGE_SIZE = 50
 
-TOKEN_FILE = Path.home() / '.github'
-STATE_FILE = Path.home() / '.varlib' / 'ghissues.json'
-
 GITHUB_GRAPHQL_URL = 'https://api.github.com/graphql'
-
-def main():
-    gh = GitHub(TOKEN_FILE)
-    try:
-        with STATE_FILE.open() as fp:
-            state = json.load(fp)
-    except FileNotFoundError:
-        state = {}
-    events = []
-    new_state = {}
-    for repo in gh.get_user_repos(USER):
-        try:
-            repo_state = state.pop(repo.id)
-        except KeyError:
-            repo_state = {
-                "issues": repo.issues.get_latest_cursor(),
-                "prs": repo.prs.get_latest_cursor(),
-                "discussions": repo.discussions.get_latest_cursor(),
-            }
-            events.append(repo.new_event)
-        else:
-            ### TODO: Cut down on code duplication here:
-            issues, new_cursor = repo.issues.get_new(repo_state["issues"])
-            if new_cursor is not None:
-                repo_state["issues"] = new_cursor
-            events.extend(
-                i for i in issues if i.author != USER or not SKIP_SELF_REPORTED
-            )
-            prs, new_cursor = repo.prs.get_new(repo_state["prs"])
-            if new_cursor is not None:
-                repo_state["prs"] = new_cursor
-            events.extend(
-                p for p in prs if p.author != USER or not SKIP_SELF_REPORTED
-            )
-            try:
-                discurse = repo_state["discussions"]
-            except KeyError:
-                discurse = repo_state["discussions"] = repo.discussions.get_latest_cursor()
-            discs, new_cursor = repo.discussions.get_new(discurse)
-            if new_cursor is not None:
-                repo_state["discussions"] = new_cursor
-            events.extend(
-                d for d in discs if d.author != USER or not SKIP_SELF_REPORTED
-            )
-        repo_state["fullname"] = repo.fullname
-        new_state[repo.id] = repo_state
-    for repo_state in state.values():
-        events.append(RepoRemovedEvent(
-            timestamp = nowstamp(),
-            repo_fullname = repo_state["fullname"],
-        ))
-    events.sort()
-    if events:
-        report_events(events)
-    STATE_FILE.parent.mkdir(parents=True, exist_ok=True)
-    STATE_FILE.write_text(json.dumps(new_state))
-
-def report_events(events):
-    #print('\n\n'.join(map(str, events)))
-    msg = EmailMessage()
-    msg['Subject'] = SUBJECT
-    msg['To'] = RECIPIENT
-    msg.set_content('\n\n'.join(map(str, events)))
-    subprocess.run(['sendmail', '-i', '-t'], input=bytes(msg), check=True)
 
 class GitHub:
     def __init__(self, token_file: Path):
@@ -267,54 +188,3 @@ class APIException(Exception):
         else:
             msg += json.dumps(resp, sort_keys=True, indent=4)
         return msg
-
-
-# The `timestamp` attributes of the following classes are used for sorting:
-
-@dataclass
-class NewIssueoidEvent:
-    type_name: ClassVar[str]
-    timestamp: str
-    repo_fullname: str
-    number: int
-    title: str
-    author: str
-    url: str
-
-    def __str__(self):
-        return (
-            f'[{self.repo_fullname}] {self.type_name} #{self.number}:'
-            f' {self.title} (@{self.author})\n<{self.url}>'
-        )
-
-
-class NewIssueEvent(NewIssueoidEvent):
-    type_name = "ISSUE"
-
-
-class NewPREvent(NewIssueoidEvent):
-    type_name = "PR"
-
-
-class NewDiscussEvent(NewIssueoidEvent):
-    type_name = "DISCUSSION"
-
-
-class NewRepoEvent(namedtuple('NewRepoEvent', 'timestamp repo_fullname url')):
-    def __str__(self):
-        return (
-            f'Now tracking repository {self.repo_fullname}\n'
-            f'<{self.url}>'
-        )
-
-
-class RepoRemovedEvent(namedtuple('RepoRemovedEvent', 'timestamp repo_fullname')):
-    def __str__(self):
-        return f'Repository {self.repo_fullname} not found; no longer tracking'
-
-
-def nowstamp():
-    return time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
-
-if __name__ == '__main__':
-    main()
